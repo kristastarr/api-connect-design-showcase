cfcd237fa01ebceddee4dd93b28b96990ff29cb0
Krista-Starr <krista.starr@ibm.com>
2023-04-27 16:12:57 -0700

    basic page structure and layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,171 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+// import PageSection from "./components/PageSection";
+import { Grid, Column } from '@carbon/react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+      {/*One*/}
+      <Grid fullWidth className="grid--black">
+        <Column lg={16} md={8} sm={4} className="section--black">
+          <Grid>
+            <Column lg={6} className="col1">
+              <h1>API Connect</h1>
+              <h2>in Cloud Pak for Integration</h2>
+            </Column>
+            <Column lg={10} className="col1">
+              <h1>image</h1>
+            </Column>
+          </Grid>
+        </Column>
+      </Grid>
+
+
+      {/*Two*/}
+      <Grid fullWidth className="grid--black">
+        <Column lg={16} md={8} sm={4} className="section--black">
+          <Grid>
+            <Column lg={{ span: 10, offset: 3 }} className="col2">       
+              <div className="video"></div>
+            </Column>
+          </Grid>
+        </Column>
+      </Grid>
+
+      
+
+      {/*Three*/}
+      <Grid fullWidth className="grid--black">
+        <Column lg={16} md={8} sm={4} className="section--black">
+          <Grid>
+            <Column lg={6} className="col1">
+               <h2>Powering essential solutions</h2>
+            </Column>
+             <Column lg={10} className="col1">
+              <p>akfj;alsdjf;alksdjf;klasdjf as;df paragraphs</p>
+            </Column>
+          </Grid>
+        </Column>
+      </Grid>
+   
+
+
+      {/*Four*/}
+      <Grid fullWidth className="grid--white">
+        <Column lg={16} md={8} sm={4} className="section--white">
+          <Grid>
+            <Column lg={6} className="col1">   
+              <h2>Supports all the modern API types</h2>
+            </Column>
+            <Column lg={10} className="col1">
+              <h1>image</h1>
+            </Column>
+          </Grid>
+        </Column>
+      </Grid>
+
+
+
+      {/*Five*/}
+      <Grid fullWidth className="grid--white">
+        <Column lg={16} md={8} sm={4} className="section--white">
+          <Grid>
+            <Column lg={10} className="col1">
+              <h2>image</h2>
+            </Column>
+            <Column lg={6} className="col1">
+              <h2>Simplified development loop</h2>
+            </Column>
+          </Grid>
+        </Column>
+      </Grid>
+
+
+
+      {/*Six*/}
+      <Grid fullWidth className="grid--white">
+        <Column lg={16} md={8} sm={4} className="section--white">
+          <Grid>
+            <Column lg={6} className="col1">
+              <h2>Automate testing with Watson AI</h2>
+            </Column>
+            <Column lg={10} className="col1">
+              <h1>image</h1>
+            </Column>
+          </Grid>
+        </Column>
+      </Grid>
+
+
+
+
+      {/*Seven*/}
+      <Grid fullWidth className="grid--white">
+        <Column lg={16} md={8} sm={4} className="section--white">
+          <Grid>
+            <Column lg={10} className="col1">       
+              <h2>image</h2>
+            </Column>
+             <Column lg={6} className="col1">
+              <h2>Monitor APIs with ease</h2>
+            </Column>
+          </Grid>
+        </Column>
+      </Grid>
+
+
+
+
+      {/*Eight*/}
+      <Grid fullWidth className="grid--white">
+        <Column lg={16} md={8} sm={4} className="section--white">
+          <Grid>
+            <Column lg={6} className="col1">
+              <h2>All in one place</h2>
+            </Column>
+            <Column lg={10} className="col1">
+              <h1>image</h1>
+            </Column>
+          </Grid>
+        </Column>
+      </Grid>
+
+
+
+      {/*Nine*/}
+      <Grid fullWidth className="grid--black">
+      <Column lg={16} md={8} sm={4} className="section--black">
+        <Grid>
+          <Column lg={6} className="col1">
+          <h2>The loop guides us</h2>
+          </Column>
+           <Column lg={10} className="col1">
+          <h1>infinity image</h1>
+          <p>al;kfj;alsdjf;aklsjdf;lasdf;j;alksjdf</p>
+          </Column>
+        </Grid>
+      </Column>
+    </Grid>
+
+
+    {/*Ten*/}
+    <Grid fullWidth className="grid--black">
+      <Column lg={16} md={8} sm={4} className="section--black">
+        <Grid>
+          <Column lg={6} className="col1">
+          <h2>Customers love us</h2>
+          </Column>
+           <Column lg={10} className="col1">
+          <h1>some quotes</h1>
+          </Column>
+        </Grid>
+      </Column>
+    </Grid>
+
+    <footer></footer>
+
+  </div>)
+};
 
 export default App;
---
+++ src/components/PageSection/PageSection.js
@@ -0,0 +1,9 @@
+import styles from './page-section.module.scss';
+
+const PageSection = (props) => {
+	return (
+		<div><h2>Page section</h2></div>
+	)
+} 
+
+export default PageSection
