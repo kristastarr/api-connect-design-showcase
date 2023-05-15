73b41ee947ff920317791f65f6523e6923d7d6dd
Krista-Starr <krista.starr@ibm.com>
2023-05-15 15:36:16 -0700

    remove all animations and transitions for users who prefer reduced motion

--- src/components/SectionNine/SectionNine.js
+++ src/components/SectionNine/SectionNine.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import Lottie from "lottie-react";
+import './section-nine.scss'
 
 import LoopAnimation from '../../lotties/loop-animation.json'
 import loop from '../../images/loop.png';
 
-import  useScrollPosition  from "../../utils/useScrollPosition";
-
 const SectionNine = () => {
-  let scrollPosition = useScrollPosition()
 	return (   
     <section className="container--outer outer--loop section--nine">
       <div className="container--inner inner--loop">
         <div className="col--40">
           <h3 className="section--black_h3">The loop guides us</h3>      
         </div>
-        <div className="col--60">          
-            {/*<img className={"loop-img"} src={loop} /> */}
+        <div className="col--60">
+          <div className="loop-img__container">          
+            <img className={"loop-img"} src={loop} alt="The design thinking loop - Make, Observe, Reflect" /> 
+          </div>
             <Lottie className="loop--lottie" animationData={LoopAnimation} loop={true} />
             <p className="conclusion-p">During our research and design process, we developed domain knowledge about the complexities of APIs in a world that is increasingly reliant on them.</p> 
             <p className="conclusion-p">We built a thorough understanding of how software developers think to balance solving their current pain points with anticipating their future needs.</p> 
