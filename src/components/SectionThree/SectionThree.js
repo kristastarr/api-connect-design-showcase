b164867856a76d10e2748523bf5ee19b28da6d40
kristastarr <krista.starr@ibm.com>
2023-08-30 11:56:33 -0700

    update reference to EDT

--- src/components/SectionThree/SectionThree.js
+++ src/components/SectionThree/SectionThree.js
@@ -15,7 +15,7 @@ const SectionThree = () => {
         <div className="col--60">
           <p className="intro-p">With an increasing need for virtual services during the pandemic, APIs (application programming interfaces) continue to be the backbone for crucial infrastructures by connecting applications and data sources.</p>
           <p className="intro-p"><span className="bold"> IBM API Connect</span>, as a part of Cloud Pak for Integration, enables software developers to create, manage, and share APIs in a single, unified experience. Through shared assets and automated testing, our solution improves API quality and slashes development cycles from weeks to days.</p>
-          <p className="intro-p">Following our own IBM Design Thinking loop, we completed extensive research to understand our users' pain points and build empathy to design and deliver an elegant product.</p>
+          <p className="intro-p">Following our own IBM Enterprise Design Thinking loop, we completed extensive research to understand our users' pain points and build empathy to design and deliver an elegant product.</p>
         </div>   
       </div>
     </section>
